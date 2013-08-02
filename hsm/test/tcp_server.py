9d4a55d8a470e0b6a44246aaf5bb7d0f47a4f195
Enea Bionda <eneabio1974@gmail.com>
2013-08-02 17:07:23 +0200

    Merge

--- hsm/test/tcp_server.py
+++ hsm/test/tcp_server.py
@@ -1,7 +1,7 @@
 from hsm import actor
 from hsm import runtime
 from hsm import iomonitor
-import http_parser
+from http_parser.parser import HttpParser
 
 import unittest
 import socket
@@ -15,9 +15,11 @@ class TestContextTop(actor.TopState):
                 self._address = address
                 self._parent = parent
                 self._body = ""
-                self._to_send = 0
-                self._sent = 0
                 self._reply = ""
+                self._total_sent = 0
+                self._reply_size = 0
+                self._http_parser = HttpParser()
+                assert self._http_parser is not None
 
         def _enter(self):
                 iomonitor.monitor_except(self._socket, self)
@@ -27,7 +29,12 @@ class TestContextTop(actor.TopState):
                 self._parent.send_remove_context(self)
                 self._socket  = None
                 self._address = None
-                self._http_parser = http_parser.parser()
+                self._http_parser = None
+
+        def on_data_except(self, socket):
+                iomonitor.unmonitor(socket)
+                self._log.error("Except")
+                self.send_fini()
 
 @actor.initial_state
 class TestContextWaitingRequest(TestContextTop):
@@ -39,58 +46,65 @@ class TestContextWaitingRequest(TestContextTop):
                 iomonitor.unmonitor_incoming(self._socket)
 
         def on_data_incoming(self, socket):
+                p = self._http_parser
                 data = socket.recv(4096)
                 print """---\n%s---\n"""% data
                 if data == "":
                         iomonitor.unmonitor(self._socket)
+                        self._log.error("Read error")
                         self.send_fini()
+                        return
                 recved = len(data)
                 nparsed = p.execute(data, recved)
                 assert nparsed == recved
                 if p.is_headers_complete():
-                        print p.get_headers()
+                        pass
 
                 if p.is_partial_body():
                         self._body.append(p.recv_body())
 
                 if p.is_message_complete():
-                        self.transition()
+                        self.transition(TestContextSending)
 
-        def on_data_except(self, socket):
-                pass
+class TestContextSending(TestContextTop):
 
-
-class TestContextBuildResponse(TestContextTop):
         def _enter(self):
-                iomonitor.monitor_outgoing(self._socket)
+                iomonitor.monitor_outgoing(self._socket, self)
 
         def _exit(self):
                 iomonitor.unmonitor(self._socket)
 
-        def on_data_outgoing(self, socket):
-                msg = ""
-                self._to_send = len(msg)
-                self._sent = 0
+@actor.initial_state
+class TestContextBuildResponse(TestContextSending):
 
-                #build msg here
+        def on_data_outgoing(self, socket):
+                def build_reply(msg):
+                        contentLen = len(msg)
+                        return "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: %s\n\n%s\n" % (contentLen, msg)
+
+                self._reply = build_reply("<html><head></head><body>Hello World</body></html>")
+                self._reply_size = len(msg)
                 self.transition(TestContextSendResponse)
 
-        def on_data_except(self, socket):
-                self.send_fini()
-
-class TestContextSendResponse(TestContextTop):
-
-        def _enter(self):
-                iomonitor.monitor_outgoing(self._socket)
-
-        def _exit(self):
-                iomonitor.unmonitor(self._socket)
+class TestContextSendResponse(TestContextSending):
 
         def on_data_outgoing(self, socket):
-                pass
-
-        def on_data_except(self, socket):
-                pass
+                reply = self._reply
+                total_sent = self._total_sent
+                to_send = reply[total_sent:]
+                self._log.trace("about to send %s" % to_send)
+                sent = self._socket.send(to_send)
+                self._log.trace("sent: %s" % sent)
+                self._reply = to_send
+                if sent == 0:
+                        self._log.error("socket closed")
+                        self.send_fini()
+                        return
+                self._total_sent = total_sent + sent
+                if self._total_sent == self._reply_size:
+                        iomonitor.unmonitor_outgoing(self._socket)
+                        self._log.info("Message sent")
+                        self.send_fini()
 
 # Server ######################################################################
 
@@ -127,8 +141,10 @@ class TcpServerIdle(TcpServer):
                 self.transition(TcpServerRunning)
 
 class TcpServerError(TcpServer):
+
         def _enter():
                 pass
+
         def _exit():
                 pass
 
@@ -137,15 +153,16 @@ class TcpServerRunning(TcpServer):
         def _enter(self):
                 iomonitor.monitor_incoming(self._acceptor, self)
                 iomonitor.monitor_except(self._acceptor, self)
-                print "Server is running"
+                self._log.info("Server is accepting at: %s %s", self._host, self._port)
 
         def _exit(self):
                 iomonitor.unmonitor(self._acceptor)
 
         def on_data_incoming(self, socket):
                 sock_addr_tuple = socket.accept()
-                self._log.info( "Accepted client from addr:%s", sock_addr_tuple[1] )
+                sock_addr_tuple[0].setblocking(0)
                 context = self._Context(sock_addr_tuple, self)
+                self._log.info( "Accepted client from addr:%s", sock_addr_tuple[1] )
                 self._context_index[context] = context
 
         def on_data_except(self, socket):
@@ -159,7 +176,8 @@ class TcpServerRunning(TcpServer):
 
 
 
-print "test tcp server"
+from hsm import default_logger
+default_logger.setLevel(default_logger.INFO)
 server = TcpServer("localhost", 8080, 16, TestContextTop)
 server.send_start()
 while True:
--- hsm/test/test_actor2.py
+++ hsm/test/test_actor2.py
@@ -2,9 +2,11 @@
 # Licenced under the MIT license
 # see LICENCE.txt
 
+import unittest
+
 from hsm import actor
 from hsm import runtime
-import unittest
+
 
 #Test state hierarchy:
 # ObjTopState
@@ -16,112 +18,111 @@ import unittest
 #     - ObjBState *
 
 class ObjTopState(actor.TopState):
-
     def on_fatal_error(self):
-	print "FatalError"
-	self.transition(ObjErrorState)
+        print "FatalError"
+        self.transition(ObjErrorState)
 
     def _enter(self):
-	print "enter %s State" % (self.__class__.__name__, )
+        print "enter %s State" % (self.__class__.__name__, )
 
     def _exit(self):
-	print "exit %s State" % (self.__class__.__name__, )
+        print "exit %s State" % (self.__class__.__name__, )
+
 
 class ObjErrorState(ObjTopState):
-
     def _enter(self):
-	print "enter %s State" % (self.__class__.__name__, )
+        print "enter %s State" % (self.__class__.__name__, )
 
     def _exit(self):
-	print "exit %s State" % (self.__class__.__name__, )
+        print "exit %s State" % (self.__class__.__name__, )
+
 
 @actor.initial_state
 class ObjAState(ObjTopState):
-
     def _enter(self):
-	print "enter %s State" % (self.__class__.__name__, )
+        print "enter %s State" % (self.__class__.__name__, )
 
     def _exit(self):
-	print "exit %s State" % (self.__class__.__name__, )
+        print "exit %s State" % (self.__class__.__name__, )
 
-    #def on_update(self):
-	#self.transition(ObjRightState)
+        #def on_update(self):
+        #self.transition(ObjRightState)
+
 
 @actor.initial_state
 class ObjBState(ObjAState):
-
     def _enter(self):
-	print "enter %s State" % (self.__class__.__name__, )
+        print "enter %s State" % (self.__class__.__name__, )
 
     def _exit(self):
-	print "exit %s State" % (self.__class__.__name__, )
+        print "exit %s State" % (self.__class__.__name__, )
 
-    #def on_update(self):
-	#self.transition(ObjRightState)
+        #def on_update(self):
+        #self.transition(ObjRightState)
+
 
 class ObjCState(ObjTopState):
-
     def _enter(self):
-	print "enter %s State" % (self.__class__.__name__, )
+        print "enter %s State" % (self.__class__.__name__, )
 
     def _exit(self):
-	print "exit %s State" % (self.__class__.__name__, )
+        print "exit %s State" % (self.__class__.__name__, )
+
 
 @actor.initial_state
 class ObjDState(ObjCState):
-
     def _enter(self):
-	print "enter %s State" % (self.__class__.__name__, )
+        print "enter %s State" % (self.__class__.__name__, )
 
     def _exit(self):
-	print "exit %s State" % (self.__class__.__name__, )
+        print "exit %s State" % (self.__class__.__name__, )
+
 
 class ObjEState(ObjCState):
-
     def _enter(self):
-	print "enter %s State" % (self.__class__.__name__, )
+        print "enter %s State" % (self.__class__.__name__, )
 
     def _exit(self):
-	print "exit %s State" % (self.__class__.__name__, )
+        print "exit %s State" % (self.__class__.__name__, )
+
 
 class ActorTest2(unittest.TestCase):
-
     def test_self_transition(self):
-	obj = ObjTopState()
-	st = obj.get_state()
-	self.assertTrue(ObjBState == st)
+        obj = ObjTopState()
+        st = obj.get_state()
+        self.assertTrue(ObjBState == st)
 
     def test_transition_to_current_state(self):
-	obj = ObjTopState()
-	obj.transition(ObjBState)
-	runtime.dispatch_all_msg()
-	st = obj.get_state()
-	self.assertTrue(ObjBState == st)
+        obj = ObjTopState()
+        obj.transition(ObjBState)
+        runtime.dispatch_all_msg()
+        st = obj.get_state()
+        self.assertTrue(ObjBState == st)
 
     def test_different_parent_transition(self):
-	obj = ObjTopState()
-	obj.transition(ObjCState)
-	runtime.dispatch_all_msg()
-	st = obj.get_state()
-	self.assertTrue(ObjDState == st)
+        obj = ObjTopState()
+        obj.transition(ObjCState)
+        runtime.dispatch_all_msg()
+        st = obj.get_state()
+        self.assertTrue(ObjDState == st)
 
     def test_ancestor_transition(self):
-	obj = ObjTopState()
-	obj.transition(ObjTopState)
-	runtime.dispatch_all_msg()
-	st = obj.get_state()
-	self.assertTrue(ObjBState == st)
+        obj = ObjTopState()
+        obj.transition(ObjTopState)
+        runtime.dispatch_all_msg()
+        st = obj.get_state()
+        self.assertTrue(ObjBState == st)
 
     def test_msg_send(self):
-	obj = ObjTopState()
-	obj.send_fatal_error()
-	runtime.dispatch_all_msg()
-	st = obj.get_state()
-	self.assertTrue(ObjErrorState == st)
+        obj = ObjTopState()
+        obj.send_fatal_error()
+        runtime.dispatch_all_msg()
+        st = obj.get_state()
+        self.assertTrue(ObjErrorState == st)
 
     def test_fini(self):
-	obj = ObjTopState()
-	obj.send_fini()
-	runtime.dispatch_all_msg()
-	st = obj.get_state()
-	print st
+        obj = ObjTopState()
+        obj.send_fini()
+        runtime.dispatch_all_msg()
+        st = obj.get_state()
+        print st
